9dd31c6f02b445257db94562719c7b533b51e3be
Amanda Cristina <amandacristinacorrea@alunos.utfpr.edu.br>
2026-06-15 14:57:18 -0300

    TESTE
    TESTE

--- Programa.c
+++ Programa.c
@@ -1,17 +1,5 @@
 /******************************************************************************
 
-Uma pequena empresa realiza vendas de produtos e serviços para diversos clientes. Atualmente, o controle financeiro é realizado manualmente, dificultando o acompanhamento dos recebimentos, das vendas parceladas e da situação financeira da empresa. Para melhorar esse processo, será desenvolvido um sistema de controle financeiro em Linguagem C (ANSI C). O sistema deverá permitir o cadastro de clientes, o registro de vendas, o acompanhamento das parcelas a receber e a geração de informações gerenciais para auxiliar na tomada de decisão.
-O principal objetivo é controlar o fluxo de entrada de dinheiro na empresa, independentemente da forma de pagamento utilizada. A empresa realiza vendas somente para clientes cadastrados. Cada venda deverá possuir um valor total e uma forma de pagamento. Dependendo da forma de pagamento escolhida, o comportamento do sistema deverá variar:
-Dinheiro (recebido imediatamente)
-Pix (recebido imediatamente)
-Cartão à Vista (recebido imediatamente, com taxa)
-Cartão Parcelado (recebido dividido em parcelas mensais, com taxa)
-O sistema deverá permitir que a empresa acompanhe:
-quanto já recebeu;
-quanto ainda tem a receber;
-quais parcelas estão vencidas;
-quais clientes possuem débitos pendentes;
-qual o faturamento total da empresa.
 
 
 *******************************************************************************/
