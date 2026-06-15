c682d5d56d8467c38255de262d6a01a07fa6ec13
voidesu <150299448+voidesu@users.noreply.github.com>
2026-06-15 14:28:16 -0300

    Update Programa.c

--- Programa.c
+++ Programa.c
@@ -1,9 +1,18 @@
 /******************************************************************************
 
-Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
+Uma pequena empresa realiza vendas de produtos e serviços para diversos clientes. Atualmente, o controle financeiro é realizado manualmente, dificultando o acompanhamento dos recebimentos, das vendas parceladas e da situação financeira da empresa. Para melhorar esse processo, será desenvolvido um sistema de controle financeiro em Linguagem C (ANSI C). O sistema deverá permitir o cadastro de clientes, o registro de vendas, o acompanhamento das parcelas a receber e a geração de informações gerenciais para auxiliar na tomada de decisão.
+O principal objetivo é controlar o fluxo de entrada de dinheiro na empresa, independentemente da forma de pagamento utilizada. A empresa realiza vendas somente para clientes cadastrados. Cada venda deverá possuir um valor total e uma forma de pagamento. Dependendo da forma de pagamento escolhida, o comportamento do sistema deverá variar:
+Dinheiro (recebido imediatamente)
+Pix (recebido imediatamente)
+Cartão à Vista (recebido imediatamente, com taxa)
+Cartão Parcelado (recebido dividido em parcelas mensais, com taxa)
+O sistema deverá permitir que a empresa acompanhe:
+quanto já recebeu;
+quanto ainda tem a receber;
+quais parcelas estão vencidas;
+quais clientes possuem débitos pendentes;
+qual o faturamento total da empresa.
+
 
 *******************************************************************************/
 #include <stdio.h>
